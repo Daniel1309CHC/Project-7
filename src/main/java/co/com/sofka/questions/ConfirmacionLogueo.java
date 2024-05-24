169e5730cb0d7e303cd45f3b6944c5991be47a33
JorgeRodriguez <josroga1@gmail.com>
2024-05-24 15:48:31 -0500

    se repara errores

--- src/main/java/co/com/sofka/questions/ConfirmacionLogueo.java
+++ src/main/java/co/com/sofka/questions/ConfirmacionLogueo.java
@@ -1,5 +1,6 @@
 package co.com.sofka.questions;
 
+import co.com.sofka.ui.PageLlenarFormulario;
 import co.com.sofka.ui.PagePrincipal;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
@@ -7,7 +8,7 @@ import net.serenitybdd.screenplay.Question;
 public class ConfirmacionLogueo implements Question<String> {
     @Override
     public String answeredBy(Actor actor) {
-        return PagePrincipal.BTN_CERRAR_SESION.resolveFor(actor).getText();
+        return PageLlenarFormulario.BTN_CREAR.resolveFor(actor).getText();
 
     }
     public static ConfirmacionLogueo confirmacionLogueo(){
