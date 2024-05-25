ce852a97a7046c8bb1d9ac66ada7be2d31038d49
JorgeRodriguez <josroga1@gmail.com>
2024-05-24 20:29:51 -0500

    se reparan errores

--- src/test/java/co/com/sofka/stepdefinitions/SetUp.java
+++ src/test/java/co/com/sofka/stepdefinitions/SetUp.java
@@ -5,20 +5,32 @@ import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
 public class SetUp {
-    @Managed(driver = "edge")
+    @Managed(driver = "chrome")
     protected WebDriver webDriver;
 
     private void setUpActor(WebDriver webDriver){
         OnStage.setTheStage(new OnlineCast());
+
+
         theActorCalled("Jorge").can(BrowseTheWeb.with(webDriver));
 
     }
 
     protected void actorSetUpTheBrowser(){
+
+        ChromeOptions chrome_options = new ChromeOptions();
+        chrome_options.addArguments("--headless");
+        chrome_options.addArguments("--no-sandbox");
+        chrome_options.addArguments("--disable-dev-shm-usage");
+        webDriver = new ChromeDriver(chrome_options);
         setUpActor(webDriver);
         maximizar(webDriver);
     }
